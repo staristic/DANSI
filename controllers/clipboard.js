6237e8147976b4bf8bb74c8b37c6f978f312f49b
staristic <dnadnaacgt@gmail.com>
2020-05-12 21:03:11 +0900

    temporary solution of copy and paste on macOS

--- controllers/clipboard.js
+++ controllers/clipboard.js
@@ -1,10 +1,24 @@
-// c++ addon of clipboard
-const addon = require('../addon/build/Release/addon');
+const platform = process.platform;
+if (platform === 'win32') {
+  // c++ addon of clipboard
+  const addon = require('../addon/build/Release/addon');
 
-module.exports.writeDataIntoClipBoardData = (data) => {
-  addon.writeDataIntoClipBoardData(data);
-};
+  module.exports.writeDataIntoClipBoardData = (data) => {
+    addon.writeDataIntoClipBoardData(data);
+  };
 
-module.exports.getClipBoardData = async () => {
-  return addon.getClipBoardData();
-};
+  module.exports.getClipBoardData = async () => {
+    return addon.getClipBoardData();
+  };
+} else if (platform === 'darwin') {
+  // TODO: resolve clipboard access problem on macOS
+  let tempClipboardOnMac = '';
+
+  module.exports.writeDataIntoClipBoardData = (data) => {
+    tempClipboardOnMac = data;
+  };
+
+  module.exports.getClipBoardData = async () => {
+    return tempClipboardOnMac;
+  };
+}
